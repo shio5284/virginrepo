387ed8bdb7c351e7e10a5d55f9db98cf656da731
shio5284 <franciscodelacruz5284@gmail.com>
2017-09-07 00:38:25 +0800

    create

---
+++ agrigit/agrigit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(agrigit.Startup))]
+namespace agrigit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
